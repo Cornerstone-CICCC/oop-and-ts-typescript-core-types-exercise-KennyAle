d3f355b69ab57f7ae70287d33221188b29441cde
KennyAle <kennyviver@gmail.com>
2025-02-05 12:30:50 -0800

    Exercise

--- src/ex04.ts
+++ src/ex04.ts
@@ -1,8 +1,14 @@
 // Exercise: Create a function `formatInput` that accepts either a string or number as input.
 // If it's a number, return it multiplied by 100. If it's a string, return it in uppercase.
 
-function formatInput(input) {
-  
+function formatInput(input: string | number): string | number {
+    if (typeof input === 'string') {
+        return input.toUpperCase()
+    } else if (typeof input === 'number') {
+        return input * 100
+    } else {
+        return 'Not a type'
+    }
 }
 
 console.log(formatInput(5));    // Expected output: 500
